a56619f32b760d3d00975688b73f29c2656b05be
hien <phanquochien@gmail.com>
2011-04-15 00:14:41 +0700

    Updating while loop examples

--- while.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl -w
-$count = 5 ;
-while ($count <100){
-	$count +=5;
-	print $count."\n";
-}
--- while1.pl
+++ while1.pl
@@ -1,3 +1,8 @@
-#!usr/bin/perl
-while (<STDIN>){print};
- 
+#!/usr/bin/perl -w
+#Author: Mr.Hien
+#Desc: While loop example 1
+$count = 5 ;
+while ($count <100){
+	$count +=5;
+	print $count."\n";
+}
