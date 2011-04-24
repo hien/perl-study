5748a4d2c9643ce4e8afe2339bca4879d01cfa15
hien <phanquochien@gmail.com>
2011-04-24 16:33:15 +0700

    Fix comment

--- check_valid_ipv4.pl
+++ check_valid_ipv4.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #Author: Mr.Hien
 #Date: Apr 8 2011
-#Purpose: Checking IPv4 Address.
+#Purpose: Checking valid IPv4 Address.
 
 chomp($input =<>);
 unless ($input =~ /^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$/) {
